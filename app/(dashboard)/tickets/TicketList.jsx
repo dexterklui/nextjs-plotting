5f03eb5f118477ccf08048fa6fe8858ef34ce69d
Dexter Lui <dexlui@pm.me>
2023-10-16 16:02:16 +0900

    feat: fetch all tickets from supabase

--- app/(dashboard)/tickets/TicketList.jsx
+++ app/(dashboard)/tickets/TicketList.jsx
@@ -1,15 +1,16 @@
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import Link from "next/link";
 
 async function getTickets() {
-  // imitate delay
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const supabase = createServerComponentClient({ cookies });
+  const { data, error } = await supabase.from("Tickets").select();
 
-  const res = await fetch("http://localhost:4000/tickets", {
-    next: {
-      revalidate: 0,
-    },
-  });
-  return res.json();
+  if (error) {
+    console.log(error.message);
+  }
+
+  return data;
 }
 
 export default async function TicketList() {
